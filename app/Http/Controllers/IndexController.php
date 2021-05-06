1ab7c6861fe3452c2366a28598168e895fd69334
Andrei <andreisr0105@gmail.com>
2021-05-06 15:59:38 -0500

    Version 2.0.0

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
@@ -9,5 +10,10 @@ class IndexController extends Controller
     public function Welcome()
     {
         return view('welcome');
+    }
+
+    public function index()
+    {
+        return Inertia::render('index');
     }
 }
